10133f9ce40efee4469a046d808fa7fffc275790
Leo Luo <luojunwenbashen@163.com>
2019-04-03 14:03:46 +0800

    create blob directly from text

--- AdventureWorks.Web/App_Start/Startup.LogStartTime.cs
+++ AdventureWorks.Web/App_Start/Startup.LogStartTime.cs
@@ -19,7 +19,6 @@ namespace AdventureWorks.Web
 
         public async Task LogStartTimeAsync()
         {
-            string sourceFile = null;
             var storageConnectionString = ConfigurationManager.ConnectionStrings["CloudStorage"].ConnectionString;
 
             if (CloudStorageAccount.TryParse(storageConnectionString, out var storageAccount))
@@ -38,23 +37,14 @@ namespace AdventureWorks.Web
                     await cloudBlobContainer.SetPermissionsAsync(permissions);
 
                     var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
-                    var localPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                    var localFileName = $"HelloWorld_{timestamp}.txt";
-                    sourceFile = Path.Combine(localPath, localFileName);
+                    var fileName = $"HelloWorld_{timestamp}.txt";
 
-                    File.WriteAllText(sourceFile, $"{timestamp},{Environment.MachineName}");
-
-                    var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(localFileName);
-                    await cloudBlockBlob.UploadFromFileAsync(sourceFile, FileMode.OpenOrCreate);
+                    var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+                    await cloudBlockBlob.UploadTextAsync($"{timestamp},{Environment.MachineName}");
                 }
                 catch (Exception ex)
                 {
                     Log.Error("Error returned from the service: {0}", ex);
-                }
-                finally
-                {
-                    if (File.Exists(sourceFile))
-                        File.Delete(sourceFile);
                 }
             }
             else
