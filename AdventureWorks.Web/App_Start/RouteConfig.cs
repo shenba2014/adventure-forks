c59e097a97f9163277e6a13a38c337604b324782
leo.luo <luojunwenbashen@163.com>
2019-04-14 07:37:42 +0800

    fix department page

--- AdventureWorks.Web/App_Start/RouteConfig.cs
+++ AdventureWorks.Web/App_Start/RouteConfig.cs
@@ -11,9 +11,11 @@ namespace AdventureWorks.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+	        routes.MapMvcAttributeRoutes();
+
+			routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ AdventureWorks.Web/Controllers/DepartmentViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdventureWorks.Services.HumanResources;
+
+namespace AdventureWorks.Web.Controllers
+{
+	[RoutePrefix("Departments")]
+    public class DepartmentViewController : Controller
+    {
+		// GET: Departments
+	    [Route]
+		public ActionResult Index()
+	    {
+		    DepartmentService departmentService = new DepartmentService();
+		    var departmentGroups = departmentService.GetDepartments();
+
+		    return View(departmentGroups);
+	    }
+
+		// GET: Departments/Employees/{id}
+		[Route("{id}")]
+		public ActionResult Employees(int id)
+	    {
+		    DepartmentService departmentService = new DepartmentService();
+		    var departmentEmployees = departmentService.GetDepartmentEmployees(id);
+		    var departmentInfo = departmentService.GetDepartmentInfo(id);
+
+		    ViewBag.Title = "Employees in " + departmentInfo.Name + " Department";
+
+		    return View(departmentEmployees);
+	    }
+	}
+}
